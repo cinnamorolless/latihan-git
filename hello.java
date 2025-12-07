3771a28eaa15db564e33c980b6c208466e1e7a68
Chandanara Nailakhansa <chandanaranailla@gmail.com>
2025-12-07 21:33:45 +0700

    Ini commit kedua: Penambahan komentar

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@
 // Commit kedua: tambah komentar
 public class hello {
     public static void main (String[] args) {
-        System.out.println("Hi this is keonho gf!!");
+        System.out.println("Hello World");
     }
 }
